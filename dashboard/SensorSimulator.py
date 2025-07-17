3514294b64f900f3df11e9ba81b796be71a3891d
al-c95 <137837298+al-c95@users.noreply.github.com>
2025-07-17 23:12:21 +1000

    set up high temperature alerts

--- dashboard/SensorSimulator.py
+++ dashboard/SensorSimulator.py
@@ -18,14 +18,20 @@ def generate_fake_sensor_data():
 
 
 def main():
+    temperature = 27.0
     while True:
-        data = generate_fake_sensor_data()
+        #data = generate_fake_sensor_data()
         try:
+            data = {
+                "temperature": temperature,
+                "humidity": 50
+            }
             response = requests.post(API_URL, json=data)
             print(f"[{datetime.now()}] Sent: {data} | Status: {response.status_code}")
         except Exception as e:
             print(f'Error sending data: {e}')
         
+        temperature += 0.1
         time.sleep(3)
 
 
--- dashboard/app.py
+++ dashboard/app.py
@@ -6,7 +6,19 @@ from sse_starlette.sse import EventSourceResponse
 import asyncio
 from datetime import datetime
 import json
+from CsvManager import CsvManager
+from fastapi import FastAPI, HTTPException
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import os
 
+
+HIGH_TEMP_THRESHOLD = 27.5
+LOW_TEMP_THRESHOLD = 12.5
+HIGH_HUMIDITY_THRESHOLD = 70
+# set these!
+FROM_EMAIL = ''
+SENDGRID_API_KEY = ''
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -20,17 +32,54 @@ class SensorData(BaseModel):
     temperature: float
     humidity: int
 
+
 current_data = {
     "temperature": "-",
+    "last_update": "-",
+    "high_temperature": "-",
+    "high_temperature_time": "-",
+    "low_temperature": "-",
+    "low_temperature_time": "-",
     "humidity": "-",
-    "last_update": "-"
 }
 
+
+def send_alert(previous_temperature, updated_temperature):
+    if (previous_temperature == '-' or float(previous_temperature) < HIGH_TEMP_THRESHOLD) and updated_temperature >= HIGH_TEMP_THRESHOLD:
+        alert_message = Mail(
+            from_email=FROM_EMAIL,
+            to_emails=[FROM_EMAIL],
+            subject='Weather Sensor Alert - 9/13 Curzon St, Ryde NSW 2112',
+            plain_text_content=f'Alert: Temperature has exceeded high threshold of {HIGH_TEMP_THRESHOLD}°C'
+        )
+        try:
+            sg = SendGridAPIClient(SENDGRID_API_KEY)
+            response = sg.send(alert_message)
+            print(response)
+        except Exception as e:
+            print(f'Failed to send alert: {str(e)}')
+
 @app.post("/update-sensor-data")
-async def receive_data(sensorData: SensorData):
-    current_data["temperature"] = sensorData.temperature
-    current_data["humidity"] = sensorData.humidity
-    current_data["last_update"] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+async def receive_data(sensor_data: SensorData):
+    updated_temperature = sensor_data.temperature
+    high_temperature = current_data["high_temperature"]
+    low_temperature = current_data["low_temperature"]
+    previous_temperature = current_data["temperature"]
+    timestamp = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    current_data["temperature"] = updated_temperature
+    current_data["humidity"] = sensor_data.humidity
+    current_data["last_update"] = timestamp
+
+    if high_temperature=="-" or updated_temperature > high_temperature:
+        current_data["high_temperature"] = updated_temperature
+        current_data["high_temperature_time"] = timestamp
+
+    if low_temperature=="-" or updated_temperature < low_temperature:
+        current_data["low_temperature"] = updated_temperature
+        current_data["low_temperature_time"] = timestamp
+
+    send_alert(previous_temperature, updated_temperature)
 
     return {"status": "success"}
 
@@ -43,3 +92,12 @@ async def update_events(request: Request):
             await asyncio.sleep(0.1)
 
     return EventSourceResponse(update_event_generator())
+
+@app.get("/connection-status-sse")
+async def connection_status(request: Request):
+    async def connection_status_generator():
+        while True:
+            # TODO: poll sensor station
+            pass
+
+    return EventSourceResponse(connection_status_generator())
