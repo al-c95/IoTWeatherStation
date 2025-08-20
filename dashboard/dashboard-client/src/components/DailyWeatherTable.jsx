2dcb7d9bae14e3e66c0180eaebf8ac9f3112095a
al-c95 <137837298+al-c95@users.noreply.github.com>
2025-08-20 15:48:54 +1000

    daily weather table component

---
+++ dashboard/dashboard-client/src/components/DailyWeatherTable.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './DailyWeatherTable.css';
+import { formatTemperature } from '../utils/formatters'
+
+function DailyWeatherTable({ data }) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Day</th>
+                    <th>Month</th>
+                    <th>Year</th>
+                    <th>Minimum</th>
+                    <th>Maximum</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((row) => (
+                    <tr key={row.id}>
+                        <td>{row.day}</td>
+                        <td>{row.month}</td>
+                        <td>{row.year}</td>
+                        <td>{formatTemperature(row.min_temp)}</td>
+                        <td>{formatTemperature(row.max_temp)}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default DailyWeatherTable;
