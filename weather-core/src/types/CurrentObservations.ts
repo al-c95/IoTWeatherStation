61e14990e44b712db532185c64a9171bd372720c
al-c95 <137837298+al-c95@users.noreply.github.com>
2026-06-20 12:23:58 +1000

    feat(weather-core): introduce wind data types and current THP and rain observations types

--- weather-core/src/types/CurrentObservations.ts
+++ weather-core/src/types/CurrentObservations.ts
@@ -1,4 +1,27 @@
-import { Temperature, Humidity, Pressure, NullableDate, Precipitation } from "./DailyWeather";
+import { Temperature, Humidity, Pressure, NullableDate, Precipitation, WindSpeed, WindDirection } from "./DailyWeather";
+
+type CurrentThpObservations = {
+    temp: Temperature;
+    humidity: Humidity;
+    dewPoint: Temperature;
+    mslPressure: Pressure;
+    timestamp: NullableDate;
+}
+
+type CurrentRainfallObservations = {
+    totalPrecipitation: Precipitation;
+}
+
+type CurrentWindObservations = {
+    meanSpeed: WindSpeed,
+    gusts: WindSpeed,
+    direction: WindDirection
+}
+
+type HighestWindGust = {
+    speed: WindSpeed,
+    direction: WindDirection
+}
 
 type CurrentObservations = {
     temp: Temperature;
--- weather-core/src/types/DailyWeather.ts
+++ weather-core/src/types/DailyWeather.ts
@@ -3,6 +3,8 @@ export type Humidity = number | null;
 export type NullableDate = Date | null;
 export type Precipitation = number | null;
 export type Pressure = number | null;
+export type WindSpeed = number | null;
+export type WindDirection = 'N' | 'NNE' | 'NE' | 'ENE' | 'E' | 'ESE' | 'SE' | 'SSE' | 'S' | 'SSW' | 'SW' | 'WSW' | 'W' | 'WNW' | 'NW' | 'NNW';
 
 export interface DailyWeather
 {
@@ -12,4 +14,6 @@ export interface DailyWeather
     minTemp: Temperature;
     maxTemp: Temperature;
     precipitation: Precipitation;
+    maxWindGustSpeed: WindSpeed;
+    maxWindGustDirection: WindDirection;
 }
